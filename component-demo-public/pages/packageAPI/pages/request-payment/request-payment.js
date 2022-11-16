e6438c984d3c396dd020ed3e72013355890b3f67
bnbot <74913490+bnbot@users.noreply.github.com>
2022-11-16 11:42:23 +0800

    feat: update component-demo-public(4.1.0) (#24)

--- component-demo-public/pages/packageAPI/pages/request-payment/request-payment.js
+++ component-demo-public/pages/packageAPI/pages/request-payment/request-payment.js
@@ -15,7 +15,7 @@ Page({
       url: 'https://nezha-mock.fe.qa1fdg.net/order',
       responseType: 'text',
       dataType: 'json',
-      success: async  (res) => { // extends outer scope this
+      success: async function (res) {
         const { timeStamp, certSn, merchantId, noncestr, paySign, prepayId } = res.data.data.option
         const paymentResult = await bn.requestPayment({
           certSn,
